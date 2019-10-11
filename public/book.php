48cb5a708f433503f30311e1523a9ca149bdf31c
Côme de Mauroy <demauroyco@gmail.com>
2019-10-11 10:56:08 +0200

    Step 01 CSS

--- public/book.php
+++ public/book.php
@@ -8,26 +8,28 @@
     <title>Checkpoint PHP 1</title>
 </head>
 <body>
-
 <?php include 'header.php'; ?>
-
 <main class="container">
 
     <section class="desktop">
-        <img src="image/whisky.png" alt="a whisky glass" class="whisky"/>
-        <img src="image/empty_whisky.png" alt="an empty whisky glass" class="empty-whisky"/>
-
-        <div class="pages">
-            <div class="page leftpage">
-                Add a bribe
-                <!-- TODO : Form -->
+        <div class="flex_container">
+            <div class="img_container">
+                <img src="image/whisky.png" alt="a whisky glass" class="whisky"/>
+                <img src="image/empty_whisky.png" alt="an empty whisky glass" class="empty-whisky"/>
             </div>
 
-            <div class="page rightpage">
-                <!-- TODO : Display bribes and total paiement -->
+            <div class="pages">
+                <div class="page leftpage">
+                    Add a bribe
+                    <!-- TODO : Form -->
+                </div>
+
+                <div class="page rightpage">
+                    <!-- TODO : Display bribes and total paiement -->
+                </div>
             </div>
+            <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
         </div>
-        <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
     </section>
 </main>
 </body>
