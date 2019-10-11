1304859093a40e2a16a1b1a93a9236e85a2e6099
Côme de Mauroy <demauroyco@gmail.com>
2019-10-11 11:22:52 +0200

    Step 02 Database and table

--- public/book.php
+++ public/book.php
@@ -8,7 +8,19 @@
     <title>Checkpoint PHP 1</title>
 </head>
 <body>
+<?php require_once '../connec.php' ?>>
 <?php include 'header.php'; ?>
+<?php
+$pdo = new \PDO(DSN, USER, PASS);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$query = 'SELECT * FROM bribe';
+$statement = $pdo->query($query);
+$bribes = $statement->fetchAll(PDO::FETCH_ASSOC);
+var_dump($bribes);
+$summary = array_column($bribes, 'payment');
+$totalPayment = array_sum($summary);
+
+?>
 <main class="container">
 
     <section class="desktop">
@@ -25,7 +37,29 @@
                 </div>
 
                 <div class="page rightpage">
-                    <!-- TODO : Display bribes and total paiement -->
+                    <table>
+                        <thead>
+                        <tr>
+                            <th colspan="2">Bribes</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        foreach ($bribes as $bribe) {
+                            echo '<tr>
+                                   <td>' . $bribe['name'] . '</td>
+                                   <td>' . $bribe['payment'] . '</td>
+                                 </tr>';
+                        }
+                        ?>
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <th>Total</th>
+                            <td><<?php echo $totalPayment ?></td>
+                        </tr>
+                        </tfoot>
+                    </table>
                 </div>
             </div>
             <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
